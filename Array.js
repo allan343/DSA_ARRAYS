191fa21fb970fc63ab75369563695697ee3699bb
Allan Cheng <allanhfcheng@gmail.com>
2020-06-24 18:39:15 -0700

    I think it works

--- Array.js
+++ Array.js
@@ -1,10 +1,10 @@
-const memory = require('./Memory.js')();
+const memory = require('./Memory.js');
 class Array {
     constructor() {
         this.length = 0;
         this._capacity = 0;
        
-        this.ptr = memory().allocate(this.length);
+        this.ptr = memory.allocate(this.length);
     }
 
     push(value) {
@@ -12,18 +12,18 @@ class Array {
             this._resize((this.length + 1) * Array.SIZE_RATIO);
         }
     
-       memory().set(this.ptr + this.length, value);
+       memory.set(this.ptr + this.length, value);
         this.length++;
     }
 
     _resize(size) {
         const oldPtr = this.ptr;
-        this.ptr = new memory().allocate(size);
+        this.ptr = memory.allocate(size);
         if (this.ptr === null) {
             throw new Error('Out of memory');
         }
-        memory().copy(this.ptr, oldPtr, this.length);
-       memory().free(oldPtr);
+        memory.copy(this.ptr, oldPtr, this.length);
+       memory.free(oldPtr);
         this._capacity = size;
     }
 
@@ -31,14 +31,14 @@ class Array {
         if (index < 0 || index >= this.length) {
             throw new Error('Index error');
         }
-        return memory().get(this.ptr + index);
+        return memory.get(this.ptr + index);
     }
 
     pop() {
         if (this.length == 0) {
             throw new Error('Index error');
         }
-        const value = memory().get(this.ptr + this.length - 1);
+        const value = memory.get(this.ptr + this.length - 1);
         this.length--;
         return value;
     }
@@ -52,8 +52,8 @@ class Array {
             this._resize((this.length + 1) * Array.SIZE_RATIO);
         }
 
-        memory().copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-        memory().set(this.ptr + index, value);
+        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+        memory.set(this.ptr + index, value);
         this.length++;
     }
 
@@ -61,7 +61,7 @@ class Array {
         if (index < 0 || index >= this.length) {
             throw new Error('Index error');
         }
-        memory().copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
         this.length--;
     }
 }
